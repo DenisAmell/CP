3cb95068ef0023263fe5d214725d98f6e78434c9
Lupapupa137 <121352602+Lupapupa137@users.noreply.github.com>
2023-10-17 18:10:51 +0300

    some problems with override im command_clean

---
+++ src/commands/command_clean.h
@@ -0,0 +1,69 @@
+#ifndef COMMAND_CLEAN_H
+#define COMMAND_CLEAN_H
+
+#include "command.h"
+#include "../pars.h"
+#include "../database_singleton.h"
+#include "../logger_singleton.h"
+#include "../trees/trees_type.h"
+#include <string>
+#include <string_view>
+#include <fstream>
+
+class command_clean : public command
+{
+private:
+    std::string _file_for_cleaning;
+    char* _filename_save = nullptr;
+
+public:
+//    command_clean(char* filename) :
+//            _filename_save(filename) {};
+    virtual ~command_clean() = default;
+
+public:
+    bool can_execute(std::string const&) override;
+    void execute(std::string const&) override;
+};
+
+//bool command_clean::can_execute(std::string const& request)
+//{
+//    logger_singleton::get_instance()
+//            ->get_logger()
+//            ->log("command_clear::can_execute(std::string const& request) called",
+//                  logger::severity::trace);
+//
+//    if (request.starts_with("CLEAR"))
+//    {
+//        std::vector<std::string> result = split(request, ' ');
+//
+//        if (result.size() != 2)
+//            return false;
+//
+//        std::cout << '[' << result[1] << ']' << std::endl;
+//
+//        std::string file_for_clean = result[15];
+//        _file_for_cleaning = std::move(file_for_clean.substr(0, file_for_clean.find("\r")));
+//
+//
+//        return true;
+//    }
+//    return false;
+//}
+
+//void command_clean::execute(std::string const& request)
+//{
+//    logger_singleton::get_instance()
+//            ->get_logger()
+//            ->log("command_clean::execute(std::string const& request) called",
+//                  logger::severity::trace);
+//
+//    database_singleton::get_instance()->add_collection(_pool_name,
+//                                                       _scheme_name,
+//                                                       _collection_name,
+//                                                       _tree_type);
+//    logger_singleton::get_instance()->get_logger()->log("In command ADD_COLLECTION execute called",
+//                                                        logger::severity::trace);
+//}
+
+#endif // COMMAND_CLEAN_H
